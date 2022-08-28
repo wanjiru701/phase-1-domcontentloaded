d793c3357c345c5fdc1a274ebf4474be7fdf8dfc
whitney <whitney.njogu@student.moringaschool.com>
2022-08-28 11:16:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+    console.log("This DOM has loaded");
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
